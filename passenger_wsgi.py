1e007d670fad4ca4aa8c3c19ff31f7e3eab625a0
atakhadiviom <atakhadiviom@gmail.com>
2025-11-28 11:12:01 +0400

    refactor(passenger_wsgi): improve path handling by using list and loop
    Replace multiple if conditions with a loop over a list of paths to make the code more maintainable and add additional paths for the application

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,11 +2,15 @@ import os
 import sys
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-if APP_ROOT not in sys.path:
-    sys.path.insert(0, APP_ROOT)
-APP_PKG = os.path.join(APP_ROOT, "app")
-if APP_PKG not in sys.path:
-    sys.path.insert(0, APP_PKG)
+paths = [
+    APP_ROOT,
+    os.path.join(APP_ROOT, "app"),
+    "/home/qircuitc/qircuitapp",
+    "/home/qircuitc/qircuitapp/app",
+]
+for p in paths:
+    if os.path.isdir(p) and p not in sys.path:
+        sys.path.insert(0, p)
 
 from app import create_app
 
